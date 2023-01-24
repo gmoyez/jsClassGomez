fccacb05ef89b9988aca12f6ed529202764bf69b
Moises Gomez <gmoyez@outlook.com>
2023-01-23 21:50:35 -0800

    modularization in js and other stuff

---
+++ 005-comments/script.js
@@ -0,0 +1,15 @@
+/**
+ * here 
+ * is 
+ * another comment
+ * with like a block thing
+ * usually at the top of the page
+ */
+
+
+/*
+more comment stuff
+*/
+
+// here is a comment in js
+alert("illustrating comments");
