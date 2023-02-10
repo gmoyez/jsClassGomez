a7e251ec8c36fbc71494902b8fa69726aaeb5474
Moises Gomez <gmoyez@outlook.com>
2023-02-09 22:56:55 -0800

    adding let and typeof

---
+++ 010-variable/scripts/main.js
@@ -0,0 +1,3 @@
+var fname = "Moises Gomez";
+const myHeading = document.querySelector("h1");
+myHeading.textContent = fname;
---
+++ 013-let-and-typeof/scripts/main.js
@@ -0,0 +1,24 @@
+let moyez = "gomez" 
+console.log("moyez is gomez - ", moyez)
+// string
+
+moyez = 1990
+console.log("moyez is 1990 -", moyez)
+// number
+
+moyez = true 
+console.log("moyez is true - ", moyez) 
+//boolean
+
+moyez = "gomez" 
+console.log("moyez is gomez which is a typeof ", (typeof moyez))
+
+
+moyez = 1990
+console.log("moyez is 1990 which is a typeof", (typeof moyez))
+
+
+moyez = true 
+console.log("moyez is true which is a typeof ", (typeof moyez)) 
+
+
