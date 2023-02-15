e6fa388189c41ca29a5a212277ff490a969cb2e7
Moises Gomez <gmoyez@outlook.com>
2023-02-14 20:14:48 -0800

    Fun With Funtions

---
+++ 015-functions/main.js
@@ -0,0 +1,11 @@
+//FUNctions are fun!
+
+function doMath (a, b) {
+    
+    console.log(`This is the result ${a / b}`)
+    console.log(`This is the result ${a * b}`)
+    console.log(`This is the result ${a + b}`)
+    console.log(`This is the result ${a - b}`)
+}
+
+doMath (10, 4)
