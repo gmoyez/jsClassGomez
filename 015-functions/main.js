5b3c7b5891e033d0484d50bb548f51f72c70dce6
Moises Gomez <gmoyez@outlook.com>
2023-02-14 21:32:07 -0800

    my functions

--- 015-functions/main.js
+++ 015-functions/main.js
@@ -1,14 +1,28 @@
 //FUNctions are fun!
 
-function doMath (a, b) {
-    let result = a * b
-    return result
-    // console.log(`This is the result ${a / b}`)
-    // console.log(`This is the result ${a * b}`)
-    // console.log(`This is the result ${a + b}`)
-    // console.log(`This is the result ${a - b}`)
+// function doMath (a, b) {
+//     let result = a * b
+//     return result
+//     // console.log(`This is the result ${a / b}`)
+//     // console.log(`This is the result ${a * b}`)
+//     // console.log(`This is the result ${a + b}`)
+//     // console.log(`This is the result ${a - b}`)
+// }
+
+
+// let returnValue = doMath (10, 4)
+// console.log(`this result was returned from the fucntion ${returnValue}`)
+
+function  doMath (a, b, c, d) {
+    let result = a + b + c + d
+    console.log(`this is the result ${a + b + c / d}`)
+    console.log(`this is the result ${a + b / c + d}`)
+    console.log(`this is the result ${a > b}`)
+    console.log(`this is the result ${a < b }`)
+    // console.log(`this is the result ${c = b}`)
+    // console.log(`this is the result ${b = a}`)
+    console.log(`this is the result ${b / a + d / c }`)
+    console.log(`this is the result ${(a * b) - (c * d)}`)
 }
 
-
-let returnValue = doMath (10, 4)
-console.log(`this result was returned from the fucntion ${returnValue}`)
+doMath(2, 4, 4, 2)
