7429027c7adb661d91be86a0570dc1dbe0bd4e65
Moises Gomez <gmoyez@outlook.com>
2023-02-14 20:38:54 -0800

    return of functions

--- 015-functions/main.js
+++ 015-functions/main.js
@@ -1,11 +1,14 @@
 //FUNctions are fun!
 
 function doMath (a, b) {
-    
-    console.log(`This is the result ${a / b}`)
-    console.log(`This is the result ${a * b}`)
-    console.log(`This is the result ${a + b}`)
-    console.log(`This is the result ${a - b}`)
+    let result = a * b
+    return result
+    // console.log(`This is the result ${a / b}`)
+    // console.log(`This is the result ${a * b}`)
+    // console.log(`This is the result ${a + b}`)
+    // console.log(`This is the result ${a - b}`)
 }
 
-doMath (10, 4)
+
+let returnValue = doMath (10, 4)
+console.log(`this result was returned from the fucntion ${returnValue}`)
