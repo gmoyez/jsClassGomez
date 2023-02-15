1058c92d72f9c8408d108f0038e740d2d6f74771
Moises Gomez <gmoyez@outlook.com>
2023-02-14 22:13:41 -0800

    arrow functions

--- 015-functions/main.js
+++ 015-functions/main.js
@@ -9,13 +9,16 @@
 //     // console.log(`This is the result ${a - b}`)
 // }
 
+doMoreMath = (a, b) => a * b
+    
+console.log(doMoreMath(20, 30))
 
 // let returnValue = doMath (10, 4)
-// console.log(`this result was returned from the fucntion ${returnValue}`)
+// console.log(`this result was returned from the fucntion ${result}`)
 
-function  doMath (a, b, c, d) {
-    let result = a + b + c + d
-    return result
+// function  doMath (a, b, c, d) {
+//     let result = a + b + c + d
+//     return result
     // console.log(`this is the result ${a + b + c / d}`)
     // console.log(`this is the result ${a + b / c + d}`)
     // console.log(`this is the result ${a > b}`)
@@ -24,8 +27,8 @@ function  doMath (a, b, c, d) {
     // // console.log(`this is the result ${b = a}`)
     // console.log(`this is the result ${b / a + d / c }`)
     // console.log(`this is the result ${(a * b) - (c * d)}`)
-}
+ //}
 
-let returnValue = doMath(2, 4, 4, 2)
-    console.log(`this result was returned from the function ${returnValue}`)
+// let returnValue = doMath(2, 4, 4, 2)
+//     console.log(`this result was returned from the function ${returnValue}`)
 
