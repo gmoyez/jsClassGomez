65d8628254f4bf4be905cd25e56e65d8d4003749
Moises Gomez <gmoyez@outlook.com>
2023-02-27 21:58:21 -0800

    updated the DOM

---
+++ 016-start-building/main.js
@@ -0,0 +1,10 @@
+//Update the DOM
+
+const updateDOM = (input) => {
+    const divEl = document.querySelector('#output')
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+updateDOM(100) 
