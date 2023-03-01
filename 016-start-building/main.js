3d233c59bbc42ad543adbaf90a9f6dad1d4c007e
Moises Gomez <gmoyez@outlook.com>
2023-02-28 21:28:07 -0800

    DOM updated and mileage fixed

--- 016-start-building/main.js
+++ 016-start-building/main.js
@@ -4,18 +4,18 @@ const myArr = []
 
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output')
-    const h1 = document.createElement('h1')
-    h1.textContent = input
-    divEl.appendChild(h1)
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
 }
 
-const trackMPGCost = (miles, gallons, price) => {
-    const MPG = miles/gallons 
-    const tripCost = MPG * price 
+const trackMPGCost = (miles, gallons, price = 3.79) => {
+    const MPG = Math.round(miles/gallons) 
+    const tripCost = gallons * price 
+    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
     myArr.push(MPG, tripCost)
 }
 
-
-
-updateDOM(trackMPGCost(300, 10, 5.40))
-updateDOM(trackMPGCost(320, 12, 5))
+trackMPGCost(300, 10, 5.40)
+trackMPGCost(320, 12, 5)
+updateDOM(myArr)
