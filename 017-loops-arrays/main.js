a941a30156d41d5d871dd88da61a2342fffd251b
Moises Gomez <gmoyez@outlook.com>
2023-03-01 21:05:06 -0800

    writing the average MPG using for loop

--- 017-loops-arrays/main.js
+++ 017-loops-arrays/main.js
@@ -1,6 +1,7 @@
-//Update the DOM
+//arrays and Loops
 
-const myArr = []
+const myMPG = []
+const myTripCost = []
 
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output')
@@ -11,17 +12,29 @@ const updateDOM = (input) => {
 
 const trackMPGCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons) 
-    const tripCost = gallons * price 
+    const tripCost = Math.round(gallons * price) 
     updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
-    myArr.push(MPG, tripCost)
+    myMPG.push(MPG)
+    myTripCost.push(tripCost)
 }
 
-const calculateAvg = () => {
-    const avgMPG = (myArr [0] = myArr[2]) / 2
-    updateDOM(`Average Miles per Gallon is ${avgMPG}`)
+const calculateMPGAvg = () => {
+    // const avgMPG = (myArr [0] = myArr[2]) / 2
+    // updateDOM(`Average Miles per Gallon is ${avgMPG}`)
+    let totalMPG = 0
+    for(let i = 0; i < myMPG.length; i++) {
+        totalMPG = totalMPG + myMPG[i]
+    }
+    let avgMPG = totalMPG/myMPG.length
+    updateDOM(`Average MPG is ${avgMPG}`)
 }
 
-trackMPGCost(300, 10, 5.40)
+// calculate average we need to determine total number of occurances and divide that number of occurances
+
+trackMPGCost(360, 15, 5.40)
 trackMPGCost(320, 12, 5)
-//updateDOM(myArr)
-calculateAvg()
+trackMPGCost(100, 7, 4.40)
+trackMPGCost(600, 24, 5.70)
+trackMPGCost(50, 2, 3.40)
+trackMPGCost(250, 15, 3.75)
+calculateMPGAvg()
