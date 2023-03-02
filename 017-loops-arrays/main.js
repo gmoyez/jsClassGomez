ee91bd86921212753c21a80b5e9003dc87ee4fb2
Moises Gomez <gmoyez@outlook.com>
2023-03-01 22:00:31 -0800

    my code for average trip cost

--- 017-loops-arrays/main.js
+++ 017-loops-arrays/main.js
@@ -10,7 +10,7 @@ const updateDOM = (input) => {
     divEl.appendChild(p)
 }
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons) 
     const tripCost = Math.round(gallons * price) 
     updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
@@ -19,8 +19,6 @@ const trackMPGCost = (miles, gallons, price = 3.79) => {
 }
 
 const calculateMPGAvg = () => {
-    // const avgMPG = (myArr [0] = myArr[2]) / 2
-    // updateDOM(`Average Miles per Gallon is ${avgMPG}`)
     let totalMPG = 0
     for(let i = 0; i < myMPG.length; i++) {
         totalMPG = totalMPG + myMPG[i]
@@ -29,12 +27,23 @@ const calculateMPGAvg = () => {
     updateDOM(`Average MPG is ${avgMPG}`)
 }
 
-// calculate average we need to determine total number of occurances and divide that number of occurances
+const calculateAvgCost = () => {
+    let totalCost = 0
+    for(let i = 0; i < myTripCost.length; i++) {
+        totalCost = totalCost + myTripCost[i]
+    }
+    let avgTripCost = totalCost/myTripCost.length
+    updateDOM(`Average Cost is ${avgTripCost}`)
+}
 
-trackMPGCost(360, 15, 5.40)
-trackMPGCost(320, 12, 5)
-trackMPGCost(100, 7, 4.40)
-trackMPGCost(600, 24, 5.70)
-trackMPGCost(50, 2, 3.40)
-trackMPGCost(250, 15, 3.75)
+// calculateAvgCost
+// commit message "my code for average cost"
+
+trackMPGandCost(360, 15, 5.40)
+trackMPGandCost(320, 12, 5)
+trackMPGandCost(100, 7, 4.40)
+trackMPGandCost(600, 24, 5.70)
+trackMPGandCost(50, 2, 3.40)
+trackMPGandCost(250, 15, 3.75)
 calculateMPGAvg()
+calculateAvgCost()
