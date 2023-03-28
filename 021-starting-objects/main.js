68ab478b836b3b74094f6e5212180bd88477d28a
Moises Gomez <gmoyez@outlook.com>
2023-03-27 20:47:48 -0700

    += objects

--- 021-starting-objects/main.js
+++ 021-starting-objects/main.js
@@ -2,22 +2,45 @@ const myArr = []
 
 // console.log(typeof myObj)
 // console.log(typeof myArr) 
-// console.log(myArr ===myObj) 
-// const myNumber = 9
-const mycarMake = {
-    make: 'Dodge',
-    model: 'Charger',
-    year: 2013,
-    fav_sports: ['baseball','soccer','football'],
+// console.log(myArr === myObj) 
+const myNumber = 9
+const myObj = {
+    name: 'Moises',
+    age: 32,
+    likes: 0,
+    hobbies: ['coding','soccer','football'],
     mobile: {
         make: 'iPhone 11 Pro Max',
         OS: 'iOS'
     },
-    myGreeting: function() {
-           
-        return `My car is a ${this.make} ${this.model} made in ${this.year}`
-    } 
+    myGreeting: function(person) {
+        return `${this.name} who loves to ${this.hobbies[0]} says what UP! ${person}`
+    },
+    increaseLikes: function(){
+        this.likes += 1
+    }             
+}
+const rioObj = myObj
+rioObj.increaseLikes(rioObj)
+console.log(rioObj)
+rioObj.age = 32
+
+
+const greeting = myObj.myGreeting('jane')
+console.log(greeting)
+
+console.log(myObj.mobile.make.length)
+
+for(key in myObj){
+    console.log(`key ${key} and value ${myObj[key]} `)
 }
 
-const greeting = mycarMake.myGreeting("")
-console.log(greeting)
+
+
+
+
+
+// comment where you played around with these methods
+
+
+// topics prim and obj <> looping over obj passed by value or refrence
