c4625f946d9530af2264f4f79d49e7ad102241b4
Moises Gomez <gmoyez@outlook.com>
2023-05-01 21:15:35 -0700

    refactor and hardcopying tables

---
+++ 025-creating-table-w-code/README.md
@@ -0,0 +1,5 @@
+# Week #9 Creating Table with Code
+
+## First Assignmnet
+
+I think both ways must serve a purpose but i do think that the "function" make s it look cleaner especially if i want or need to refactor.
---
+++ 025-creating-table-w-code/main.js
@@ -0,0 +1,71 @@
+const FORM = document.getElementById('form-input')
+const ERR = document.getElementById('err')
+const AVG_OUTPUT = document.getElementById('output-avg')
+
+const MY_DATA = []
+
+function updateDOM (input, id) {
+    const divEl = document.querySelector(id)
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+function trackMPGandCost (miles, gallons, price) {
+    const MPG = Math.round(miles/gallons) 
+    const tripCost = Math.round(gallons * price) 
+    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`, '#output')
+    return {
+        MPG: MPG,
+        tripCost: tripCost,
+        miles: miles,
+        gallons: gallons,
+        price: price
+    }
+}
+
+function calculateAvg () {
+    let sumMPG = 0
+    let sumTripCost = 0
+    MY_DATA.forEach(obj => {
+        sumMPG += obj.MPG
+        sumTripCost += obj.tripCost
+    })
+    let avgMPG = Math.round(sumMPG/MY_DATA.length)
+    let avgTripCost = Math.round(sumTripCost/MY_DATA.length)
+        updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
+        updateDOM(`Average Trip is ${avgTripCost}`, '#output-avg')
+}
+
+function isFormValid (miles, gallons, price) {
+    const errMsg = []
+    if (miles === 0 || gallons === 0 || price === 0) {
+        errMsg.push('Make sure you input value greater than 0!!, Try Again')
+    }
+    if (price > 1000) {
+        errMsg.push('Really!!!?? I think this is an error...Try again')
+    }
+    if (errMsg.length > 0) {
+        ERR.textContent = errMsg 
+    } else {
+        return true
+    }
+}
+
+FORM.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const miles = parseInt(e.target.miles.value)
+    const gallons = parseInt(e.target.gallons.value)
+    const price = parseInt(e.target.price.value)
+
+    const isValid = isFormValid(miles, gallons, price)
+    if(isValid) {
+        ERR.textContent = ''
+        AVG_OUTPUT.textContent = ''
+        const dataObj = trackMPGandCost(miles, gallons, price)
+        MY_DATA.push(dataObj)
+        calculateAvg()
+    }
+    FORM.reset()
+
+})
