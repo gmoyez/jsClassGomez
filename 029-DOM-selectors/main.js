379e707ac1654aa45353fce4141744c59fa48820
Moises Gomez <gmoyez@outlook.com>
2023-05-15 20:37:26 -0700

    fixed code

---
+++ 029-DOM-selectors/main.js
@@ -0,0 +1,32 @@
+import { renderTable } from "./render.js"
+import { getTripData, saveTripData } from "./storage.js"
+import { isFormValid, trackMPGandCost, calculateAvg } from "./handleinput.js"
+
+const FORM = document.getElementById('form-input')
+const ERR = document.getElementById('err')
+const AVG_OUTPUT = document.getElementById('output-avg')
+
+const MY_DATA = getTripData()
+renderTable(MY_DATA)
+
+
+
+FORM.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const miles = parseFloat(e.target.miles.value)
+    const gallons = parseFloat(e.target.gallons.value)
+    const price = parseFloat(e.target.price.value)
+
+    const isValid = isFormValid(miles, gallons, price)
+    if(isValid) {
+        ERR.textContent = ''
+        AVG_OUTPUT.textContent = ''
+        const dataObj = trackMPGandCost(miles, gallons, price)
+        MY_DATA.push(dataObj)
+        saveTripData(MY_DATA)
+        renderTable(MY_DATA)
+        calculateAvg(MY_DATA)
+    }
+    FORM.reset()
+
+})
---
+++ 029-DOM-selectors/storage.js
@@ -0,0 +1,14 @@
+function getTripData(MY_DATA) {
+    const tripDataJSON = localStorage.getItem('tripdata')
+    if(tripDataJSON !== null) {
+      return JSON.parse(tripDataJSON)  
+    } else {
+        return []
+    }   
+}
+
+function saveTripData(MY_DATA) {
+    localStorage.setItem('tripdata', JSON.stringify(MY_DATA))
+}
+
+export {getTripData, saveTripData}
