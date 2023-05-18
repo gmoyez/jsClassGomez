0c765e43328ac771bdfed32768bed443fd55ba1e
Moises Gomez <gmoyez@outlook.com>
2023-05-17 20:31:23 -0700

    working code

--- 031-intro-to-oop/main.js
+++ 031-intro-to-oop/main.js
@@ -1,21 +1,47 @@
 class Person {
     constructor(firstName, lastName, address){
-        this.firstName = firstName
-        this.lastName = lastName
-        this.address = address
+        this._firstName = firstName
+        this._lastName = lastName
+        this._address = address
     }
+    getFirstName () {
+        return `${this._firstName}`    
+    }
+    setFirstName (newFirstName) {
+        this._firstName = newFirstName
+    }
+    getLastName (){
+        return `${this._lastName}`
+    }
+    setLastName(newLastName){
+        this._lastName = newLastName 
+    }
+    getAddress() {
+        return `${this._address}`
+    }
+    setAddress(newAddress) {
+        this._address = newAddress
+    }
+
+
     showStatus () {
-        console.log(`${this.firstName} lives at ${this.address}`)
+        console.log(`${this.getFirstName()} lives at ${this.getAddress()}`)
     }
 }
 
 class Student extends Person {
     constructor(firstName, lastName, address, classList) {
         super(firstName, lastName, address)
-        this.classList = classList
+        this._classList = classList
     }
-    showStatus() {
-        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`)
+    getClassList () {
+        return `${this._classList}`
+    }
+    setAddress (newClassList) {
+        this._classList = newClassList
+    }
+    showStatus () {
+        console.log(`${this.getFirstName()} lives at ${this.getAddress()} is taking ${this.getClassList()}`)
     }
 
 }
@@ -24,3 +50,5 @@ const moises = new Student("Moises", "Gomez", "123 no way i'll share it", ["Eng1
 moises.showStatus()
 const john = new Person("John", "Doe", "789 chase field ")
 john.showStatus()
+
+console.log(moises.getFirstName()) 
